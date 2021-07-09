ab369087705829325f4cd67b3390cc4b673bfc3e
Владимир <55278600+VladimirShat@users.noreply.github.com>
2021-07-09 20:20:54 +0500

    Update Wobble.cs

--- Assets/Glass + Liquid/Wobble.cs
+++ Assets/Glass + Liquid/Wobble.cs
@@ -5,15 +5,16 @@ using UnityEngine;
 public class Wobble : MonoBehaviour
 {
     public Renderer rend;
-    Vector3 lastPos;
-    Vector3 velocity;
-    Vector3 lastRot;  
-    Vector3 angularVelocity;
-
     public float MaxWobble = 0.03f;
     public float WobbleSpeed = 1f;
     public float Recovery = 1f;
-
+    
+    Vector3 lastPos;
+    Vector3 velocity;
+    
+    Vector3 lastRot;  
+    Vector3 angularVelocity;
+    
     float wobbleAmountX;
     float wobbleAmountZ;
     float wobbleAmountToAddX;
@@ -21,14 +22,15 @@ public class Wobble : MonoBehaviour
     float pulse;
     float time = 0.5f;
     
-    // Use this for initialization
     void Start()
     {
         rend = GetComponent<Renderer>();
     }
+    
     private void Update()
     {
         time += Time.deltaTime;
+        
         // decrease wobble over time
         wobbleAmountToAddX = Mathf.Lerp(wobbleAmountToAddX, 0, Time.deltaTime * (Recovery));
         wobbleAmountToAddZ = Mathf.Lerp(wobbleAmountToAddZ, 0, Time.deltaTime * (Recovery));
